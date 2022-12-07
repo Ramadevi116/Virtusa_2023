89cf985aa22011990b0f38fa78a643eac7611c33
Ramadevi116 <89075054+Ramadevi116@users.noreply.github.com>
2022-12-07 14:37:30 +0530

    Create add-2-num.ts

---
+++ Angular/add-2-num.ts
@@ -0,0 +1,11 @@
+<div ng-app="">
+    <h3>Using Input Type Number</h3>
+
+    <p>First Number:
+        <input type="number" ng-model="c" />
+    </p>
+    <p>Second Number:
+        <input type="number" ng-model="d" />
+    </p>
+    <p>Sum: {{ c + d }}</p>
+</div>
