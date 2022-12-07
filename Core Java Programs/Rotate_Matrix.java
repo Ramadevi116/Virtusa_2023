c26c40cf123dd5dd68416be2bb46ea10506295d2
Ramadevi116 <89075054+Ramadevi116@users.noreply.github.com>
2022-12-07 14:50:16 +0530

    Create Rotate_Matrix.java

---
+++ Core Java Programs/Rotate_Matrix.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class TUF {
+    static int[][] rotate(int[][] matrix) {
+        int n = matrix.length;
+        int rotated[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        return rotated;
+    }
+
+    public static void main(String args[]) {
+        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int rotated[][] = rotate(arr);
+        System.out.println("Rotated Image");
+        for (int i = 0; i < rotated.length; i++) {
+            for (int j = 0; j < rotated.length; j++) {
+                System.out.print(rotated[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
